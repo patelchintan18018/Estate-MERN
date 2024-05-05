924132c432baa8e07577c43e17851f78683cc8f8
Chintan <patelchintan18018@gmail.com>
2024-05-05 17:42:12 +0530

    create header/navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      <Header/>
         <Routes>
           <Route path="/" element ={<Home/>} />
           <Route path="/about" element ={<About/>} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <>
+        <header className='bg-slate-200'>
+            <div className='flex justify-between items-center mx-auto p-3 max-w-6xl'>
+                <h1 className='font-bold flex-wrap sm:text-2xl'>
+                    <span className='text-slate-500'>Sahand</span>
+                    <span className='text-slate-700'>Estate</span>
+                </h1>
+                <form className='bg-slate-100 flex p-3 items-center rounded-lg '>
+                    <input type="text" placeholder='Search ...' className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                    <FaSearch className='text-slate-600'/>
+                </form>
+                <ul className='flex gap-5'>
+                    <Link to='/' className='hidden sm:inline text-slate-700 font-semibold hover:font-bold'><li>Home</li></Link>
+                    <Link to='/about' className='hidden sm:inline text-slate-700 font-semibold hover:font-bold'><li>About</li></Link>
+                    <Link to='/signup' className='text-slate-700 font-semibold hover:font-bold'><li>Sign In</li></Link>
+                </ul>
+            </div>
+        </header>
+    </>
+  )
+}
