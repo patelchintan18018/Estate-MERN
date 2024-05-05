2fc5b38349624c2b6289a2dc19bfbe74a40a3f48
Chintan <patelchintan18018@gmail.com>
2024-05-05 18:18:41 +0530

    Dababase connection

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODATABASE)
+  .then(() => console.log("Database connected ...."))
+  .catch((err) => console.log("Unable to connect database ..." , err));
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server is running on 3000 !!!!')
-})
+  console.log("Server is running on 3000 !!!!");
+});
