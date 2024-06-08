523632a4f0810d474e96b55a857e9d7042c9dcc1
Chintan <patelchintan18018@gmail.com>
2024-06-08 12:45:34 +0530

    footer blog

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaEnvelope, FaPhone } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
