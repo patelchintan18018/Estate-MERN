49678b77177e39776afbc4e37925ebb282f8fa43
Chintan <patelchintan18018@gmail.com>
2024-06-08 12:29:27 +0530

    footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -13,23 +13,23 @@ const Footer = () => {
           <p className='flex gap-4 items-center'><FaPhone /> +1 234 567 890</p>
         </div>
         <nav className="mb-4 md:mb-0 flex-1">
-          <ul className="flex flex-row space-y-2 md:space-y-0 md:space-x-4">
-            <li><a href="/about" className="hover:text-white hover:scale-105 transition-all duration-300 ease-in-out">About</a></li>
-            <li><a href="/contact" className="hover:text-white hover:scale-105 transition-all duration-300 ease-in-out">Contact</a></li>
-            <li><a href="/faq" className="hover:text-white hover:scale-105 transition-all duration-300 ease-in-out">FAQ</a></li>
+          <ul className="flex flex-row space-y-2 md:space-y-0 md:space-x-4 justify-center items-start">
+            <li><a href="/about" className="hover:text-white hover:scale-110 transition-all duration-300 ease-in-out">About</a></li>
+            <li><a href="/contact" className="hover:text-white hover:scale-110 transition-all duration-300 ease-in-out">Contact</a></li>
+            <li><a href="/faq" className="hover:text-white hover:scale-110 transition-all duration-300 ease-in-out">FAQ</a></li>
           </ul>
         </nav>
-        <div className="flex space-x-4 flex-1">
-          <a href="https://facebook.com" className="text-slate-500 hover:text-[#1877F2] hover:scale-105 transition-all duration-300 ease-in-out">
+        <div className="flex space-x-4 flex-1 justify-center items-start">
+          <a href="https://facebook.com" className="text-slate-500 hover:text-[#1877F2] hover:scale-110 transition-all duration-300 ease-in-out">
             <FaFacebook size="1.5em" />
           </a>
-          <a href="https://twitter.com" className="text-slate-500 hover:text-[#1DA1F2] hover:scale-105 transition-all duration-300 ease-in-out">
+          <a href="https://twitter.com" className="text-slate-500 hover:text-[#1DA1F2] hover:scale-110 transition-all duration-300 ease-in-out">
             <FaTwitter size="1.5em" />
           </a>
-          <a href="https://instagram.com" className="text-slate-500 hover:text-[#E4405F] hover:scale-105 transition-all duration-300 ease-in-out">
+          <a href="https://instagram.com" className="text-slate-500 hover:text-[#E4405F] hover:scale-110 transition-all duration-300 ease-in-out">
             <FaInstagram size="1.5em" />
           </a>
-          <a href="https://linkedin.com" className="text-slate-500 hover:text-[#0A66C2] hover:scale-105 transition-all duration-300 ease-in-out">
+          <a href="https://linkedin.com" className="text-slate-500 hover:text-[#0A66C2] hover:scale-110 transition-all duration-300 ease-in-out">
             <FaLinkedin size="1.5em" />
           </a>
         </div>
